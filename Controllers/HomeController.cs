fb8eed341b5c66df81c3959ff7d7dd1fb6f10b24
Ryan Dines <ryan.dines@gmail.com>
2019-02-28 08:58:54 -0500

    Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcClient.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult SecureData()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public async Task<JsonResult> CallAPI()
+        {
+            // get access token with OpenID Connect protocol
+            string accessToken = await HttpContext.GetTokenAsync("access_token");
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                string content = await client.GetStringAsync("http://localhost:5004/identity");
+                return new JsonResult(JArray.Parse(content));
+            }
+
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
